8311a62a1b770fc4e39acfba81d3eb0ae19df157
saykat1 <nursaykat@gmail.com>
2021-10-04 14:17:38 +0600

    basic table

---
+++ src/components/BasicTable.js
@@ -0,0 +1,41 @@
+import React, { useMemo } from "react";
+import { useTable } from "react-table";
+
+import { COLUMNS } from "./columns";
+import MOCK_DATA from "./MOCK_DATA.json";
+
+function BasicTable() {
+  const columns = useMemo(() => COLUMNS, []);
+  const data = useMemo(() => MOCK_DATA, []);
+
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    useTable({ columns, data });
+
+  return (
+    <table {...getTableProps()}>
+      <thead>
+        {headerGroups.map((headerGroup) => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map((column) => (
+              <th {...column.getHeaderProps()}>{column.render("Header")}</th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {rows.map((row) => {
+          prepareRow(row);
+          return (
+            <tr {...row.getRowProps()}>
+              {row.cells.map((cell) => (
+                <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+              ))}
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+}
+
+export default BasicTable;
